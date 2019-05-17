75f589f06dda11e76ce24423f7f4ffb92df9629f
Top Nguyễn <admin@topnguyen.com>
2019-05-17 23:17:46 +0700

    remove

---
+++ README.md
@@ -0,0 +1,19 @@
+![Logo](Logo.png)
+# Grafana.ReserveProxy
+> Author [**Top Nguyen**](http://topnguyen.net)
+
+## Overview
+Grafana.ReserveProxy is a reserve proxy for Grafana by .NET Core.
+
+Easy to you can access the Grafana in the Server hosted via localhost.
+
+## Usage
+- Just download the [build](build) folder.
+- Adjust Grafana Endpoint (`ServiceRootUrl`) in the `appsettings.json`.
+- Deploy the `build` to your Server.
+- Run the proxy by `dotnet Grafana.ReserveProxy.dll` or you can host the Proxy via IIS.
+- After you access the Proxy, if face the `Unauthenticated` or `401` issue, 
+please access `<domain>/login` to signin into the Grafana Dashboard and everything will be fine.
+
+## License
+Grafana.ReserveProxy is licensed under the [MIT License](LICENSE).
---
+++ src/Grafana.ReserveProxy/Startup.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Elect.Web.Middlewares.HttpContextMiddleware;
+using Elect.Web.Middlewares.MeasureProcessingTimeMiddleware;
+using Elect.Web.Middlewares.ReverseProxyMiddleware;
+using Elect.Web.Middlewares.ServerInfoMiddleware;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Grafana.ReserveProxy
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddElectReserveProxy(_ =>
+            {
+                _.ServiceRootUrl = "http://127.0.0.1:8080/"; // "http://127.0.0.1:3000/"
+                _.AfterReserveProxy = context => { Console.WriteLine(context.Request.GetDisplayUrl()); };
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseElectHttpContext();
+
+            app.UseElectMeasureProcessingTime();
+
+            app.UseElectServerInfo();
+
+            app.UseElectReserveProxy();
+        }
+    }
+}
