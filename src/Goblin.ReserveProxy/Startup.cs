3798137e8c114b891ea2625f761cd4c4fe6f22c7
Top Nguyen <topnguyen92@gmail.com>
2019-05-31 18:21:24 +0700

    change lib

--- src/Goblin.ReserveProxy/Startup.cs
+++ src/Goblin.ReserveProxy/Startup.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Data;
-using Elect.Core.ConfigUtils;
-using Elect.Web.Middlewares.HttpContextMiddleware;
-using Elect.Web.Middlewares.MeasureProcessingTimeMiddleware;
-using Elect.Web.Middlewares.ReverseProxyMiddleware;
-using Elect.Web.Middlewares.ServerInfoMiddleware;
+﻿using Elect.Core.ConfigUtils;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using ProxyKit;
 
 namespace Goblin.ReserveProxy
 {
@@ -24,44 +18,18 @@ namespace Goblin.ReserveProxy
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddElectReserveProxy(_ =>
-            {
-                var destinationEndpoint = Configuration.GetValueByEnv<string>("DestinationEndpoint");
-
-                bool isValidUri = Uri.TryCreate(destinationEndpoint, UriKind.Absolute, out var destinationUri);
-
-                if (!isValidUri)
-                {
-                    throw new InvalidConstraintException("Your destination domain is not valid absolute URI");
-                }
-                
-                _.ServiceRootUrl = destinationUri.ToString();
-
-                _.BeforeReserveProxy = context =>
-                {
-                    Console.WriteLine($"[Before Reserve] {context.Request.GetDisplayUrl()}");
-                    return true;
-                };
-                
-                _.AfterReserveProxy = context =>
-                {
-                    Console.WriteLine($"[After Reserve] {context.Request.GetDisplayUrl()}");
-                };
-            });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             app.UseDeveloperExceptionPage();
 
-            app.UseElectHttpContext();
+            var destinationEndpoint = Configuration.GetValueByEnv<string>("DestinationEndpoint");
 
-            app.UseElectMeasureProcessingTime();
-
-            app.UseElectServerInfo();
-
-            app.UseElectReserveProxy();
+            app.RunProxy(context => context
+                .ForwardTo(destinationEndpoint)
+                .AddXForwardedHeaders()
+                .Send());
         }
     }
 }
